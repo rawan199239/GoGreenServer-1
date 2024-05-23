3ef2ab2def3de5b0b95b16c9d98d771ef988bf68
Ahmed Hossam Elmenabawy <83036142+Elmenabawy@users.noreply.github.com>
2024-05-24 02:55:56 +0300

    Update CORS

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(helmet());
-app.use(cors());
+app.use(cors({ origin : '*'}))
 
 //user middleware(APPLICATION-LEVEL MIDDLEWARE)
 //LOGIN
