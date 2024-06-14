d4c7fbde4e90ca5fcb03d64b0e70feecae8aa749
Ahmed Hossam Elmenabawy <83036142+Elmenabawy@users.noreply.github.com>
2024-06-14 04:31:31 +0300

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const jwtSecret = config.get("jwtSecret");
+
 const bcrypt = require("bcrypt");
 const config = require("config");
 const User = require("../models/UserModel");
