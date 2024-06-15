5681cbc5627848ee5186a394330c37bf37a0e717
Ahmed Hossam Elmenabawy <83036142+Elmenabawy@users.noreply.github.com>
2024-06-15 15:44:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -44,13 +44,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(helmet());
-app.use(cors({
-  origin: 'https://gogreenserver-1-1.onrender.com', // Replace with your frontend URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-  optionsSuccessStatus: 204
-}));
+app.use(cors({ origin : '*'}))
 
 
 //user middleware(APPLICATION-LEVEL MIDDLEWARE)
