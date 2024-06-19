2044b8e6fe8937da56522d688d983e5d2e98907f
rawan199239 <160759157+rawan199239@users.noreply.github.com>
2024-06-19 04:14:37 +0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -46,6 +46,7 @@ router.post("/", validator, async (req, res) => {
       isAdmin: user.isAdmin,
       address: user.address,
       packages:user.packages,
+      predicted_consumption:user.predicted_consumption,
       kind: user.kind,
       // Include the updated consumption data
       months: months.map(month => ({ month: month, count: user[month] }))
